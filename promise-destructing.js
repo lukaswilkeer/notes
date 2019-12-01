0ce94fbc2e59e0ed904925902779a9aa43dd447b
Lukas Wilkeer (@lukaswilkeer) <lukas4l@gmail.com>
2019-12-01 01:16:17 -0200

    Revision: promise-destructing code

--- promise-destructing.js
+++ promise-destructing.js
@@ -1,14 +1,18 @@
+/*
+	Demonstrate the use of destruct on async functions.
+*/
+
 const assert = require('assert')
 
 // Fixme: Verificar especificação para resposta de erro
-const resolved = [undefined, { headers: {}, status: 200 }, {}]
+const resolved = [null, { body: {}, headers: {}, status: 200 }]
 
 const testScenario = (err, body) => {
 	if (err) console.error(err)
 
 	assert(typeof(body), 'json', 'should return body')
 	assert(body, {}, 'should return empty object')
-	assert(typeof err, 'undefined')
+	assert(typeof err, 'null')
 }
 
 const toResolve = new Promise((resolve, reject) => setTimeout(resolve(resolved), 300))
