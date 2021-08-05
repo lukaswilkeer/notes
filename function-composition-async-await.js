b72948975622f4d833300d96e47d2185e72de811
Lukas W. Santos <lukasc4l@gmail.com>
2021-08-05 01:02:15 -0300

    function-composition-async-await.js

--- function-composition-async-await.js
+++ function-composition-async-await.js
@@ -1,29 +1,56 @@
 /*
   The goal here is to demonstrate de use o assync operatios  on  a composition.
 
-  Don't forget to remember that is a composition, your data structure could/should mutate, so you should handle  errors on a specific manner
-  that don't trow away the entire composition and completes the stack with the error result.
-
-  This is easy, but not mentioned anywhere.
+  For that we create e transpose applicative that taks n numbers of functions an apply a pipe into it.
 */
 
 const assert = require('assert')
 const axios = require('axios')
 
-const add1 = (x) => (y) => x+y
+const transform = async (...args) => {
+  this.finalResult = []
+  this.intermediateResult = null;
 
+  //let self = this;
+  try {
+    if (typeof args[args.length] == 'number') {
+      this.index = args.length
+      args.pop()
+    }
+
+    if (this.index == undefined) this.index = args.length;
+  } catch (err) {
+    this.index = args.length
+  }
+
+  if (this.finalResult.length == 0) {
+    this.intermediateResult = await args[this.index];
+    this.finalResult.push(this.intermediateResult)
+  } else {
+    this.intermediateResult = await args[this.index](this.finalResult[this.finalResult.length]);
+  }
+
+  if (args.length >= this.index && this.index >= -1) {
+    this.index = this.index - 1
+    return transform(args, this.index);
+  } else {
+    this.finalResult[this.finalResult.length];
+  }
+}
+
+const add1 = (x) => (y) => x+y
+  
 const promise = (number) => new Promise((resove, reject) => {
     return setTimeout(() => number, 3000)
 })
 
-const connect = (result) => setTimeout(() => result, 3000);
+const connect = async (result) => new Promise((resolve, reject) => setTimeout(() => resolve(result), 3000));
 
 const test = (result, toAssert) => {
-	  assert(result, toAssert)
+	assert(result, toAssert)
 }
 
-const promiseWait = async () => await promsie(1)
+const promiseWait = async () => await promise(1)
 
-test(1, connect(1))
-test(2, add1(promiseWaiti(), 1))
-// test('array', typeof filter(request))
+test(1, transform(connect(1)))
+test(2, transform(add1, promiseWait(1)))
