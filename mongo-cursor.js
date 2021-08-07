0380f2a533dafd1d9e3c79e5f1ffc38dabe72ae5
Lukas W. Santos <lukasc4l@gmail.com>
2021-08-07 02:55:23 -0300

    mongo-cursor.js

---
+++ mongo-cursor.js
@@ -0,0 +1,25 @@
+/* This is a how to on iterating over the mongo cursor. */
+
+const { MongoClient, CURSOR_FLAGS } = require("mongodb");
+
+const mongoURI = 'mongodb://localhost';
+
+const client = new MongoClient(mongoURI);
+
+(async function __runMongoQueries() {
+    const connection = await client.connect(mongoURI);
+    const database = connection.db('mongomart');
+
+    const items = database.find({});
+
+    // iterating over a for loop using the iterator protocol
+    // this method exaust the cursos, good to invoke functions
+    // where you need to know the the end of the opration
+    for (const item of items) console.log(item)
+
+    // iterating assyncronouslly without exausting the cursor
+    while (items.next()) console.log(items);
+
+    // iterating syncronouslly using callbacks
+    await Promise.all(items.forEach((items) => console.log(items)));
+})();
